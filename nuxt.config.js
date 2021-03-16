6d5a504ca542c6376da9d11fefdac96863bbb49e
GakutoOkawara <ookawara0817@gmail.com>
2021-03-16 22:03:50 +0900

    autoprefixerを入れたため修正commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,26 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'learn-nuxt-ts',
+    title: "learn-nuxt-ts",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "@/assets/scss/global.scss"
-  ],
+  css: ["@/assets/scss/global.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,18 +28,34 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   styleResources: {
-    scss: ["@/assets/scss/variable.scss"]
+    scss: ["@/assets/scss/_variable.scss"]
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
+  // build: {}
+
   build: {
+    // ビルド時にSCOPED CSSを適用するために記述
+    postcss: {
+      plugins: [
+        autoprefixer({ grid: "autoplace" }),
+        purgecss({
+          content: [
+            "./pages/**/*.vue",
+            "./layouts/**/*.vue",
+            "./components/**/*.vue"
+          ],
+          whitelist: ["html", "body", "nuxt-progress"],
+          whitelistPatterns: [/(col|row)/]
+        })
+      ]
+    }
   }
-}
+};
